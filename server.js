29fbe0f9429e6712e56aea480a36df542c5a6122
DiFex-dev <derenok2002@gmail.com>
2021-12-02 18:12:23 +0300

    dfsdf

--- server.js
+++ server.js
@@ -10,11 +10,12 @@ const favicon = require('express-favicon');
 const path = require('path');
 const port = process.env.PORT || 8080;
 
-app.use(express.static(__dirname + "/build/"));
-
-app.get(/.*/, function (req, res) {
-    res.sendFile(__dirname + '/build/index.html');
-});
+if (process.env.NODE_env === "production"){
+    app.use(express.static("../client/build"));
+    app.get("*", (req, res) =>{
+        req.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+    });
+};
 
 const db = mysql.createConnection({
 host: "localhost",
